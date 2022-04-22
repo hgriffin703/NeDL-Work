6e03f0b93ac504480d197664a8c66940ab00f5bc
Hayley Griffin <hayley.griffin@conagra.com>
2022-04-22 15:47:41 -0500

    This code doesn't work but I am going to commit

--- PetProject/Program.cs
+++ PetProject/Program.cs
@@ -4,26 +4,35 @@ namespace MyApplication
 {
   class Program
   {
-    static double MilesPerWeek()
-    {
-    Console.WriteLine("How many miles do you run a week?");
-    double Mileage = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("By what percent do you want to run more miles each week? Enter as a decimal");
-    double upMileage = Convert.ToDouble(Console.ReadLine());
-    double newMileage = (Mileage*upMileage)+ Mileage;
-    
-    return newMileage;
-  }
       static void Main(string[] args)
     { //user enters the date of the race
-    double newMileage = MilesPerWeek();
+      Console.WriteLine("How many miles do you run a week?");
+      double startMileage = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("By what percent do you want to run more miles each week? Enter as a decimal");
+      double upMileage = Convert.ToDouble(Console.ReadLine());
       Console.Write("Enter a date (e.g. 10/22/1987): ");
       DateTime inputtedDate = DateTime.Parse(Console.ReadLine());
       //subtracts your race day - today to get days left
       double daysUntilRace = inputtedDate.Subtract(DateTime.Today).TotalDays;
+      double weeksUntilRace = daysUntilRace/7;
+
+      double nextWeeksMileage;
+      for (int index = 0; index < weeksUntilRace; index++)
+      {
+        double moreMilesToRun = startMileage * upMileage;
+        double eachWeeksMileage = moreMilesToRun + startMileage;
+      for (int index2 = 0; index2 <weeksUntilRace; index2++)
+      {
+        nextWeeksMileage = eachWeeksMileage * moreMilesToRun;
+      }
+      }
+      Console.WriteLine("Week " + index + " you should run " + nextWeeksMileage);
+     
+     
+     
       Console.WriteLine("Your race is on " + inputtedDate);
       Console.WriteLine("You have " + daysUntilRace + " days until your race");
-      Console.WriteLine("Next week you should run: " + newMileage);
+      Console.WriteLine("You have: " + weeksUntilRace + " weeks until the race!");
     }
   }
 }
