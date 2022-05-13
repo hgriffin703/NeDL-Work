09d30c258b55dd7dd9a68f07aa8eb43c48d76d96
Hayley Griffin <hayley.griffin@conagra.com>
2022-05-13 11:20:53 -0500

    push for review of why increment won't work

--- Tier2Week1/Competency.js
+++ Tier2Week1/Competency.js
@@ -1,17 +1,31 @@
 function addNewItem() {
-    var addToDo = "";
-    //takes the var and sets it equal to the fname that was entered
-    addToDo = document.getElementById("enterToDo").value;
-    document.getElementById("greeting").innerHTML = "Hello there " + addToDo + "!";
-    if (addToDo == "") {
-        alert("Please enter a task to add");
+    var theNewToDo = "";
+    // place the values in the form into variables
+    theNewToDo = document.forms["ToDoForm"]["enterToDo"].value;
+    // validate that something was entered as a word
+    if (theNewToDo == "") {
+        // no word was entered so tell the user
+        alert("Please enter a To Do to add");
         return false;
     }
-    //else
-    //{
-    //var tableRef = document.getElementById("ToDoList");
-    //(tableRef.insertRow(tableRef.rows.length)).innerHTML = addToDo;
-    //}
+    else {
+        var tableRef = document.getElementById("ToDoList");
+        var row = tableRef.insertRow(-1);
+        row.innerHTML = theNewToDo;
+        document.forms["ToDoForm"]["enterToDo"].value = "";
+    }
+}
+function clearoneitem() {
+    var toDoTable = document.getElementById("ToDoList");
+    var inputValue = document.forms["ToDoForm"]["clearItemValue"];
+    if (Number(inputValue.value) < 1 || Number(inputValue.value) == null || Number(inputValue.value) > toDoTable.rows.length - 1) {
+        // no number was enter; tell the user
+        alert("Please enter the row number of the To Do to remove");
+        return false;
+    }
+    else {
+        toDoTable.deleteRow(Number(inputValue.value));
+    }
 }
 function clearthelist() {
     // clear the table of all rows
--- Tier2Week1/Competency.ts
+++ Tier2Week1/Competency.ts
@@ -1,19 +1,36 @@
-function addNewItem()
+function addNewItem() 
 {
-    let addToDo: string = "";
-    //takes the var and sets it equal to the fname that was entered
-    addToDo = (<HTMLInputElement> document.getElementById("enterToDo")).value;
-    document.getElementById("greeting")!.innerHTML = "Hello there " + addToDo + "!";
-    if(addToDo == "")
-    {
-        alert("Please enter a task to add");
-        return false;
+    let theNewToDo: string = "";
+    // place the values in the form into variables
+    theNewToDo = document.forms["ToDoForm"]["enterToDo"].value;
+    // validate that something was entered as a word
+    if (theNewToDo == "") {
+      // no word was entered so tell the user
+      alert("Please enter a To Do to add");
+      return false;
     }
-    //else
-    //{
-        //var tableRef = document.getElementById("ToDoList");
-        //(tableRef.insertRow(tableRef.rows.length)).innerHTML = addToDo;
-    //}
+    else
+    {
+        let tableRef = <HTMLTableElement> document.getElementById("ToDoList");
+        var row = tableRef.insertRow(-1);
+        row.innerHTML = theNewToDo;
+        document.forms["ToDoForm"]["enterToDo"].value = "";
+     }
+}
+function clearoneitem()
+{
+  let theToDoList = document.getElementById("ToDoList") as HTMLTableElement;
+  const inputValue = document.forms["ToDoForm"]["clearItemValue"] as HTMLInputElement;
+    if (Number(inputValue.value) < 1 || Number(inputValue.value) == null || Number(inputValue.value)> theToDoList.rows.length-1) {
+        // no number was enter; tell the user
+        alert("Please enter the row number of the To Do to remove");
+        return false;
+      }
+       else
+      {
+        toDoTable.deleteRow(Number(inputValue.value));
+      } 
+
 }
 
 function clearthelist() {
