bd3287be516bd9a9a3fdc62a98cac9d353373f14
Hayley Griffin <hayley.griffin@conagra.com>
2022-05-23 09:04:32 -0500

    committing some changes

--- Tier2Week2/Day2/Bored.js
+++ Tier2Week2/Day2/Bored.js
@@ -1,66 +1,90 @@
-async function getToDo()
+async function getParticipants()
 {
     var APIstring = "https://www.boredapi.com/api/";
-    var newThingtoDo = document.getElementById("bored").value;
-    var newCost = document.getElementById("cost").value;
-    var minprice = 1;
-    var maxprice = 4;
+    var newPeople = document.getElementById("people").value;
 
-    if (newThingtoDo==1 && newCost == 0)
-    {  
-        APIstring = APIstring + "activity";
-
-         var response = await fetch(APIstring);
-        var jsonData = await response.json();
-        var theNewHobby = JSON.stringify(jsonData.activity);
-
-        document.getElementById("theHobby").innerHTML = theNewHobby; 
-
+    if(newPeople ==0)
+    {
+        alert("You must select at least 1 participant")
     }
-   else if (newThingtoDo==1 && newCost==1)
-   {
-    minprice == 1;
-   }
-   else if(newThingtoDo ==1 && newCost==2)
-   {
-       minprice == 2;
-       maxprice ==3;
-   }
+    else(newPeople == 1 || newPeople == 2 || newPeople == 3 || newPeople ==4)
+    {
 
-    APIstring = APIstring +"activity";
+    APIstring = APIstring +"activity?participants=" +newPeople;
 
     var response = await fetch(APIstring);
     var jsonData = await response.json();
     var theNewHobby = JSON.stringify(jsonData.activity);
-    theNewHobby = theNewHobby.substring(1,theNewHobby.length-1);
     document.getElementById("theHobby").innerHTML = theNewHobby;
-    var theNewCost = JSON.stringify(jsonData.participants);
-    //theNewCost = theNewCost.substring(1,theNewCost.length-1);
-    document.getElementById("theCost").innerHTML = theNewCost;
+    var theNewPeople = JSON.stringify(jsonData.participants);
+    document.getElementById("thePeople").innerHTML = theNewPeople;
+    }
 
+}
 
-/* 
-    else if (newCost==2)// the user wants a medium quote
-        {  
-         minLength = .3;
-         maxLength = .6;
+async function getToDo()
+{
+    var APIstring = "https://www.boredapi.com/api/";
+    var newCost = document.getElementById("cost").value;
+
+    var minprice = 0.0;
+    var maxprice = 0.8;
+
+    if(newCost==1)
+        {
+            minprice = 0.0;
+            maxprice = 0.2;
         }
-    else// the user wants a long one
-        minLength = .6;
+    else if(newCost == 2)
+        {
+            minprice = 0.21;
+            maxprice = 0.45;
+        }
+    else
+        {
+            minprice = 0.46;
+            maxprice = 0.79;
+        }
 
-       APIstring = APIstring + "activity";
-
-       var response = await fetch(APIstring);
-
-       var jsonData = await response.json();
-       var theNewHobby = JSON.stringify(jsonData.activity);
-       theNewHobby = theNewHobby.substring(1,theNewHobby.length-1);
-       document.getElementById("theHobby").innerHTML = theNewHobby;
-       var theNewCost = JSON.stringify(jsonData.price);
-       theNewCost = theNewCost.substring(1,theNewCost.length-1);
-       document.getElementById("theCost").innerHTML = theNewAuthor; */
+    APIstring = APIstring +"activity?minprice=" + minprice + "&maxprice=" + maxprice;
 
 
+    var response = await fetch(APIstring);
+    var jsonData = await response.json();
+    var theNewHobby = JSON.stringify(jsonData.activity);
+    document.getElementById("theHobby").innerHTML = theNewHobby;
+    var theNewCost = JSON.stringify(jsonData.price);
+    document.getElementById("thePeople").innerHTML = theNewCost;
+}
 
-   
+function savehobby()
+{
+    localStorage.setItem("content", document.getElementById("theHobby").innerHTML);
+    var content = localStorage.content;
+    if(content !== undefined)
+    {
+        var tableRef = document.getElementById("SavedHobbies");
+        (tableRef.insertRow(tableRef.rows.length)).innerHTML = content;
+    }
+}
+
+function crossoffhobby() {
+    var theToDoList = document.getElementById("myHobbyTable");
+    var inputValue = document.forms["hobbygenerator"]["clearItemValue"];
+    if ((inputValue.value) == null) {
+        // no number was enter; tell the user
+        alert("Please enter the row number of the To Do to remove");
+        return false;
+    }
+    else {
+        console.log(inputValue.value);
+        theToDoList.strike(inputValue.value);
+        document.forms["hobbygenerator"]["clearItemValue"].value = "";
+    }
+}
+
+function clearAll() {
+    // clear the table of all rows
+    var tableRef = document.getElementById("SavedHobbies");
+    tableRef.innerHTML = " ";
 }
