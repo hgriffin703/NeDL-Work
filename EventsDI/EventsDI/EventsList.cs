7646c6d61003a364a554f70e625bbee4bee0720b
Hayley Griffin <hayley.griffin@conagra.com>
2022-05-26 12:54:22 -0500

    committing som echanges

--- EventsDI/EventsDI/EventsList.cs
+++ EventsDI/EventsDI/EventsList.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace EventsDI
 {
-    internal class EventsList
+    public interface IEventsList
     {
+        public class EventsList : IEventsList
+        {
+            public List<Events> ListEvents = new List<Events>();
+
+            ListEvents.Add(new Events() { EventDate = "10/24/2022",EventType = "Wedding", EventName = "Hayley's Wedding"});
+            ListEvents.Add(new Events() { EventDate = "03/13/2023",EventType = "Wedding", EventName = "Sarah's Wedding"});
+            ListEvents.Add(new Events() { EventDate = "06/29/2023",EventType = "Graduation", EventName = "Emily's Graduation"});
+        }
     }
 }
